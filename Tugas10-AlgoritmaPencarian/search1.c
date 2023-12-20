7d31476c4f3a429b28f04d481556d90ac1a66fcb
dndrlyya <diandraaliyya20@gmail.com>
2023-12-20 11:17:40 +0700

    baru

--- Tugas10-AlgoritmaPencarian/search1.c
+++ Tugas10-AlgoritmaPencarian/search1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <time.h>
 #define SIZE 100
-int x;
+
 size_t linearSearch(const int array[], int key, size_t size);
+
 int main(void){
+
     int a[SIZE];
+    
     for (size_t x = 0; x < SIZE; ++x)
     {
         a[x] = 2 * x;
@@ -12,12 +16,17 @@ int main(void){
     int searchKey;
     scanf("%d", &searchKey);
 
+    clock_t begin = clock();
     size_t index = linearSearch(a, searchKey, SIZE);
+    clock_t end = clock();
+
     if (index != -1){
         printf("Found value at index %d\n", index);
     }else{
         puts("Value not found");
     }
+    double time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
+    printf("Time spent : %f seconds\n", time_spent);
 }
 
     size_t linearSearch(const int array[], int key, size_t size){
